8d84b3b0fdf331785cf41bc25260caba40a9e269
Kevin Hu <jiansun37@gmail.com>
2025-08-06 23:36:57 -0400

    Added MultiCameraController and MultiDisplayActivator to aid with EYEPOOL iteration, refactored Utilities folder with Util.cs

--- Assets/Scenes/EYEPOOL/Scripts/Utilities/Util.cs
+++ Assets/Scenes/EYEPOOL/Scripts/Utilities/Util.cs
@@ -1,20 +1,18 @@
 using UnityEngine;
 using System;
 
-// Utility class 
+// Miscellaneous Utility class 
 public static class Util
 {
-    /// <returns>
     ///  -1 = not inside any sink corner  
     ///   0 = top-left  (x<0 , z>0)  
     ///   1 = top-right (x>0 , z>0)  
     ///   2 = bottom-right (x>0 , z<0)  
     ///   3 = bottom-left (x<0 , z<0)
-    /// </returns>
     public static int GetSinkID(Vector3 p, float Limit)
     {
         if (Mathf.Abs(p.x) < Limit || Mathf.Abs(p.z) < Limit)
-            return -1;                      // still inside play-area
+            return -1;                      // still inside play area
 
         if (p.x < 0f && p.z > 0f) return 0;
         if (p.x > 0f && p.z > 0f) return 1;
@@ -32,7 +30,7 @@ public static class Util
         return new Vector2(xyz.x, xyz.z);
     }
 
-    public static class RandomExtensions
+    public static class RandomExtensions // allow for randomly-generated values following a normal distribution
     {
         private static bool hasSpare = false;
         private static float spare;
@@ -58,5 +56,20 @@ public static class Util
             hasSpare = true;
             return mean + stdDev * u * s;
         }
+    }
+
+    public static int[] GetShuffledArray(int N)
+    {
+        int[] randomOrder = new int[N];
+
+        for (int i = 0; i < N; i++) randomOrder[i] = i;
+
+        System.Random rng = new System.Random();
+        for (int i = 0; i < N; i++)
+        {
+            int j = rng.Next(i + 1); // randomly generate swap index
+            (randomOrder[i], randomOrder[j]) = (randomOrder[j], randomOrder[i]); // std::swap, basically
+        }
+        return randomOrder;
     }
 }
