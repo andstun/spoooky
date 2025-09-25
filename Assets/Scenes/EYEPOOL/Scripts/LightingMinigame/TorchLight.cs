7b1ba1518515c7f9f954e00fffbe19f84202c7d4
Oscar San <oscarsani007@gmail.com>
2025-09-24 23:22:10 -0400

    Interactive Candles and Reworked Campfire Features Implemented

--- Assets/Scenes/EYEPOOL/Scripts/LightingMinigame/TorchLight.cs
+++ Assets/Scenes/EYEPOOL/Scripts/LightingMinigame/TorchLight.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 using UnityEngine.Rendering;
+using Augmenta;  // Required for AugmentaObject
 
 [RequireComponent(typeof(Light))]
 public class TorchFlicker : MonoBehaviour
 {
     Light hd;
     float baseIntensity;
+    bool triggerBlackFlicker = false;
 
     void Awake()
     {
@@ -15,14 +17,47 @@ public class TorchFlicker : MonoBehaviour
 
     void Update()
     {
-        // 3  <-> speed, 2 <-> roughness of flicker
-        float n = Mathf.PerlinNoise(Time.time * 3f, 0);
-        hd.intensity = baseIntensity * Mathf.Lerp(0.7f, 1.3f, n);
+        if (!triggerBlackFlicker)
+        {
+            // 3  <-> speed, 2 <-> roughness of flicker
+            float n = Mathf.PerlinNoise(Time.time * 3f, 0);
+            hd.intensity = baseIntensity * Mathf.Lerp(0.7f, 1.3f, n);
 
-        // subtle colour shift
-        hd.color = Color.Lerp(
-            new Color(1f, 0.44f, 0.22f),   // deep orange
-            new Color(1f, 0.55f, 0.3f),    // lighter
-            n);
+            // subtle colour shift
+            hd.color = Color.Lerp(
+                new Color(1f, 0.44f, 0.22f),   // deep orange
+                new Color(1f, 0.55f, 0.3f),    // lighter
+                n);
+        }
+        else
+        {
+            // 3  <-> speed, 2 <-> roughness of flicker
+            float n = Mathf.PerlinNoise(Time.time * 8f, 0);
+            hd.intensity = baseIntensity * Mathf.Lerp(0.8f, 0.01f, n);
+        }
     }
+
+    void OnTriggerEnter(Collider other)
+    {
+        AugmentaObject augmenta = other.GetComponent<AugmentaObject>();
+        if (augmenta == null)
+        {
+            return;
+        }
+
+        triggerBlackFlicker = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        AugmentaObject augmenta = other.GetComponent<AugmentaObject>();
+        if (augmenta == null)
+        {
+            return;
+        }
+
+        triggerBlackFlicker = false;
+    }
+
+    
 }
